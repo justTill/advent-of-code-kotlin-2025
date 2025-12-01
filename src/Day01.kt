a532a0f4ad4683bb6d93e3bbd8e1e426ad8da120
Till Pilarczyk <till.pilarczyk@capgemini.com>
2025-12-01 11:45:23 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,49 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var zeroCount = 0
+        input.fold(50) { current, line ->
+            val letter = line.takeWhile { it.isLetter() }
+            val number = line.dropWhile { it.isLetter() }.toInt()
+            val newPosition = when (letter) {
+                "R" -> (current + number) % 100
+                else -> (current - number % 100 + 100) % 100
+            }
+            if (newPosition == 0) zeroCount++
+            newPosition
+        }
+        return zeroCount
     }
+
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var zeroCount = 0
+        var fullRotations = 0
+        input.fold(50) { current, line ->
+            val letter = line.takeWhile { it.isLetter() }
+            var number = line.dropWhile { it.isLetter() }.toInt()
+            fullRotations += number / 100
+            number %= 100
+            var newPosition = current
+            var addToZero = false
+
+            if(letter == "R"){
+                newPosition = current + number
+                addToZero = newPosition > 100
+            }
+            if(letter == "L"){
+                val tmp = current - number % 100
+                addToZero = tmp < 0 && current != 0
+                newPosition = tmp + 100
+            }
+            newPosition %= 100
+            if (newPosition == 0 || addToZero) {
+                zeroCount++
+            }
+            newPosition
+        }
+        return zeroCount + fullRotations
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
