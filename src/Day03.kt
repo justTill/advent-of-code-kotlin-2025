f7abb5e4abfdafb6f1e3acd6bea0420fd0b2b96a
Till Pilarczyk <till.pilarcti@web.de>
2025-12-02 21:38:51 +0100

    wip vscode

---
+++ src/Day03.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 2
+    }
+    fun part2(input: List<String>): Int {
+       return 1
+    }
+    val input = readInput("Day03")
+    part1(input).println() 
+    part2(input).println()
+}
