2d1520ba860c3b7e4fcb3e362098ff684eae7f3c
Till Pilarczyk <till.pilarcti@web.de>
2025-12-03 09:27:01 +0100

    add day 03

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,11 +1,46 @@
+
+
+import kotlin.math.max
+
+
+
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        return 2
+    fun getHightesVoltagePart1(battery: String) : Int {
+        return battery.indices.flatMap { currentVoltageIndex -> 
+            (currentVoltageIndex + 1 .. battery.length-1 ).map { nextVoltageIndex ->
+                "${battery[currentVoltageIndex]}${battery[nextVoltageIndex]}".toInt()
+            }
+        }.maxOrNull()?: throw RuntimeException("No Value Found")
+
     }
-    fun part2(input: List<String>): Int {
-       return 1
+
+
+    fun getHightesVoltagePart2(battery: String) : Long {
+        /** Nehmen wir "818181911112111" und k = 12 als Beispiel:
+            Stack leer, wir starten links: '8' → Stack: 8
+            Nächste Ziffer '1' → kleiner als Stack-Spitze → einfach hinzufügen → Stack: 8,1
+            Nächste '8' → größer als '1' und genügend Ziffern übrig → '1' entfernen → Stack: 8 → '8' hinzufügen → Stack: 8,8
+            Weiter so bis zum Ende
+            Endresultat: 888911112111 (die größte 12-stellige Zahl in Originalreihenfolge)
+         */
+        val newBatteryLength = 12
+        val newBatteryVoltage = mutableListOf<Char>()
+
+        for ((index, currentChar) in battery.withIndex()) {
+            while (newBatteryVoltage.isNotEmpty() && currentChar > newBatteryVoltage.last() && newBatteryVoltage.size + (battery.length - index) > newBatteryLength) {
+                newBatteryVoltage.removeAt(newBatteryVoltage.size - 1)
+            }
+            if (newBatteryVoltage.size < newBatteryLength) {
+                newBatteryVoltage.add(currentChar)
+            }
+        }
+        return newBatteryVoltage.joinToString("").toLong()
+
     }
+
+    fun part1(input: List<String>): Int = input.sumOf { getHightesVoltagePart1(it) }
+    fun part2(input: List<String>): Long = input.sumOf { getHightesVoltagePart2(it) }
     val input = readInput("Day03")
     part1(input).println() 
     part2(input).println()
